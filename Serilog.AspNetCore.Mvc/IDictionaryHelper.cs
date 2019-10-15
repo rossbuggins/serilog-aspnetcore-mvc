7d71aab1fdd700c2c9629521491c745ccf77c5b1
Ross Buggins <20972856+rossbuggins@users.noreply.github.com>
2019-10-15 09:27:56 +0100

    started adding unit tests for MvcLoggingExtensionMethodsTests

---
+++ Serilog.AspNetCore.Mvc/IDictionaryHelper.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Serilog.AspNetCore.Mvc
+{
+    public interface IDictionaryHelper
+    {
+        IDictionary<string, object> Add(IDictionary<string, object> values, string key, object value);
+        void Concatenate(IDictionary<string, object> values, string name1, string name2, string joinWith, string newName);
+        IDictionary<string, object> InitializeValueStore();
+    }
+}
